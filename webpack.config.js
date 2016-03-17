4f48b21ee3e00ed603ea51843ba49b23b229ed8b
Ethan Friedman <ethanjfriedman@gmail.com>
2016-03-17 13:30:54 -0400

    webpack + dependencies

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+//HTML Webpack Plugin will take the 'index.html' from our dist folder (
+// the one with the code that webpack has bundled up)
+//and inject it into the body of our app's index.html
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'    //webpack's entry point
+  ],
+  module: {
+    loaders: [
+      //loaders transform given files
+      //this is a coffeeScript transformer
+      {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+    ]
+  },
+  output: {
+    //where to output the bundled code
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
